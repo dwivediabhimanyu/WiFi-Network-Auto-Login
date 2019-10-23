c965944be39ecf76fee0ee610081c2bec0f71e81
abhidube002 <56932131+abhidube002@users.noreply.github.com>
2019-10-23 22:24:32 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import random
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
